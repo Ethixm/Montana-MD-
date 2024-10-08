225cae2c566c5e138111497f8847e0b0cbe199ad
Ethixm <Ethixm@gmail.com>
2024-10-08 18:34:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || "263789432940"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || "94759329023"  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://kenton2004:kenton2004@cluster0.8ywpe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 global.port= process.env.PORT || 5000
 global.email = 'xkenton0@gmail.com'
@@ -15,16 +15,16 @@ global.devs = '263789432940';
 global.website = 'https://github.com/montanatech/montana-md-' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3c341828d86ee7a89c73f.jpg'
 module.exports = {
-  sessionName:  process.env.SESSION_ID ||  "",  //PUT Session Id Here, 
+  sessionName:  process.env.SESSION_ID ||  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMEd3SkNpci9abWl3WnlOR2l6UVlMODVBa0hYYzNjc01GVzYyRU1PTUdHUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMVdxRVhFejZrc3BSeTVqZ2lzZXpLbjVSRTg2VzhQTXVDeU5scEE1b0VRND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ1UHJFQXNZUTV4NTlTclhSYzR5dkFQbFJwNTlWWmtnVWVPUndmZG5IZ2xvPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJsdDZ6UDI2UkZMRU8zdUNReUdxVTRWOGM2WHlmbGEwNXVpM1NVWlFrb3pvPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhHTjB1S1ZjRjg5NmpYQnd0WkMyRnpoUmd1RHc4a2FzSElHYXNQVVByMGM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBpb3JWVU9ENEpIZHpuOENMdlUvcVZIZnpnVmtBeDdJMjFndG51STdERGs9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEVZWVNOTWJONUlGSlJZaUdLaytOek85SzhpWXh0OUs4Q3Q5eE1zR2huaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidDAxRTlyOTdMMjF6cXVaWVBLbStwRDcwVjlXTzRwd0t1eDBXYVcwK1dSQT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlcrWkw1L2hydGF3Zk8vTWN0ZVFxbWpYVlJuZzQzdG9RbXFmckovanhkVmFhbTB4UkJEZFFFTGZ3NERES3FNM1JGRlBlRUp0NnBzazZ4WXNRQWRua0N3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjAxLCJhZHZTZWNyZXRLZXkiOiJ2a2p1YXFNS2pzMDJud2Z3cGxyc21YT1l1eUkzY25ackxEZ29rRmV1MjRjPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIwTnFUdXI2NVRNMmRMRGJxV2tQVUxBIiwicGhvbmVJZCI6IjM2NTUwNzdlLTEyMjAtNDAwMi1hYWY1LWZlMDU2M2QxZDJhYiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJTYk5uU2I0RlJ2OHpFYU5LRHpVWm1tcjZ4MnM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR0hvMG1hUnJQejYxRmNjUXdESmR2dVVYV0R3PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjJBOUMxN0RUIiwibWUiOnsiaWQiOiI5NDc1OTMyOTAyMzo0NEBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDTFAweE1zREVKamFsTGdHR0FzZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiVkxRVk1CTTlyYVZrQWhGdnlOakZjYzFqeGxYVlNWaVk0Q1ZUbENkRFJobz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiNWQ2OGhEQUZMRFYySmRlRHdpRW5PUUl0eURrS0twNGtNTzhLTnFGSGtocGx2ZkVlSk1sTmUvcHVQa2xDdW9wRStsS0tOTDArZkNZVE9sNWNwaVVTQlE9PSIsImRldmljZVNpZ25hdHVyZSI6ImhRbWNVcFEzV01wcDlRUkFXNlNxbkNzNWZIbkViUzlSY2VYSGJ5NmtwMmt4R0VTc3dDemJYaEQvYkVaVHNPT3dpVXVIeklwSzJaL09Rbm5CMTRYVUFBPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3NTkzMjkwMjM6NDRAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCVlMwRlRBVFBhMmxaQUlSYjhqWXhYSE5ZOFpWMVVsWW1PQWxVNVFuUTBZYSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyODM5MjQ4NywibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFFRFkifQ==",  //PUT Session Id Here, 
   author:  process.env.PACK_AUTHER ||  '',
   packname:  process.env.PACK_NAME || '',
    
   botname:   process.env.BOT_NAME === undefined ? "MONTANA" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'MAGICAL-KX' : process.env.OWNER_NAME,  
+  ownername: process.env.OWNER_NAME === undefined ? 'VIHAGA MD' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
-  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? true : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
   auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
